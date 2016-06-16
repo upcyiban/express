72dba629445b293347703d4e41cf59779b4eb9e3
chenzifeng1 <704734862@qq.com>
2016-06-16 23:21:41 +0800

    改掉了端口，并加了易班授权的信息

--- src/main/java/express/yb/upc/edu/cn/service/LoginService.java
+++ src/main/java/express/yb/upc/edu/cn/service/LoginService.java
@@ -1,12 +1,15 @@
 package express.yb.upc.edu.cn.service;
 
 import com.google.gson.Gson;
+import express.yb.upc.edu.cn.GsonTemplate.VeriyPayInfo;
 import express.yb.upc.edu.cn.confing.DevConfig;
 import express.yb.upc.edu.cn.util.MCrypt;
 import express.yb.upc.edu.cn.util.SessionUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * Created by lenovo on 2016/6/5.
@@ -17,15 +20,59 @@ public class LoginService {
     @Autowired
     private HttpSession httpSession;
 
-    public boolean isLogin() {
-        return httpSession.getAttribute("userid") != null;
+    @Autowired
+    private GetRealMessage getRealMessage;
+
+    public boolean procssAuthReal() throws IOException{
+        String access_token = (String) httpSession.getAttribute("access_token");
+        String message = getRealMessage.getMessage(access_token,"yb_wx");
+        return  saveRealSession(message);
     }
+
+
+    /**
+     * 保存学校验证信息的session
+     * @param message
+     * @return
+     */
+    public boolean saveRealSession(String message){
+        Gson gson = new Gson();
+        try {
+            VeriyPayInfo veriyPayInfo = gson.fromJson(message,VeriyPayInfo.class);
+            if(veriyPayInfo.info.equals("success")){
+                httpSession.setAttribute("yb_wx",veriyPayInfo.info.yv_wx);
+                return true;
+            }else {
+                return false;
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return  false;
+        }
+    }
+
+    /**
+     * 判断用户是否携带真实信息(学号)
+     * @return
+     */
+    public boolean isAuthReal(){ return httpSession.getAttribute("yb_studentid")!=null;}
+
 
     public String toYibanAuth() {
         String auth = "redirect:https://openapi.yiban.cn/oauth/authorize?client_id=" + DevConfig.client_id + "&redirect_uri=" + DevConfig.redirect_uri;
         return auth;
     }
 
+    public boolean isLogin() {
+        return httpSession.getAttribute("userid") != null;
+    }
+
+    /**
+     * 处理授权.成功则跳转至/,失败则重新引导用户至易班授权模块
+     * @param verify_request 易班服务器提供的信息,加密的.
+     * @return
+     * @throws Exception
+     */
     public String processAuth(String verify_request) throws Exception {
         MCrypt mCrypt = new MCrypt();
         String res = new String(mCrypt.decrypt(verify_request));
