1d7044797eb1dd42affc7d8d6ee11525a692ef60
chenzifeng1 <704734862@qq.com>
2016-06-16 23:20:50 +0800

    增加了易班接口的json解析

---
+++ src/main/java/express/yb/upc/edu/cn/GsonTemplate/VeriyPayInfo.java
@@ -0,0 +1,11 @@
+package express.yb.upc.edu.cn.GsonTemplate;
+
+/**
+ * Created by 陈子枫 on 2016/6/16.
+ */
+public class VeriyPayInfo {
+    public RealInfo info;
+    public class RealInfo{
+        public int yv_wx;
+    }
+}
